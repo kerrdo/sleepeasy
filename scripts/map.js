8d4a1c097ea4f769e770bc8217455aef0afbf1b4
Viyzed <Andrew@Andrews-Air.rgu.ac.uk>
2017-04-28 10:08:28 +0100

    Kill Me
    It’s somewhat working at least.

--- scripts/map.js
+++ scripts/map.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var map, userLocation, searchTerm, geocoder;
 
 function initMap() {
@@ -105,3 +106,21 @@ function getLocation() {
       });
     }
   }
+=======
+(function(window, google) {
+
+  //map options
+  var options = {
+    center: {
+      lat: '52.15',
+      lng: '-2.1'
+    },
+    zoom: 7
+  },
+
+  element = document.getElementById('map');
+  //map
+  map = new google.maps.Map(element, options);
+
+}(window, google));
+>>>>>>> Stashed changes
